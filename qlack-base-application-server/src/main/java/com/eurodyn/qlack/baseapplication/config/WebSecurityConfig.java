b3713c083f6cc626a267063710ffca8fd49ef5d0
george verroiopoulos <george.verroiopoulos@eurodyn.com>
2024-02-13 10:16:42 +0200

    Update Qlack Version and add csrf as dependency to the project.

--- qlack-base-application-server/src/main/java/com/eurodyn/qlack/baseapplication/config/WebSecurityConfig.java
+++ qlack-base-application-server/src/main/java/com/eurodyn/qlack/baseapplication/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eurodyn.qlack.baseapplication.config;
 
+import com.eurodyn.qlack.filter.CustomCookieFilter;
 import com.eurodyn.qlack.util.jwt.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,13 +27,12 @@ public class WebSecurityConfig {
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http
-        .csrf(AbstractHttpConfigurer::disable)
+    http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(requests -> requests
             .requestMatchers(PUBLIC_URIS).permitAll()
             .anyRequest().authenticated())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        //.addFilterBefore(customCookieFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(customCookieFilter, BasicAuthenticationFilter.class)
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
